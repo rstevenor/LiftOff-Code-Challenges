dfb77b4f2f4c21676055b0f952a4664578ca5250
Rosalyn <rosalyn.stevenor@gmail.com>
2022-04-24 16:21:42 -0500

    FirstReverse completed and tested

---
+++ Week 3/CodeChallengeWeek3.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static String FirstReverse(String str) {
+        String newStr ="";
+        for(int i = 0; i < str.length(); i++){
+             newStr = str.charAt(i) + newStr;
+        }
+        return newStr ;
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(FirstReverse(s.nextLine()));
+    }
+
+}
