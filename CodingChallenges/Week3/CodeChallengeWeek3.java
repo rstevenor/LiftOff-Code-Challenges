146f21c3b684f71d4fd26a654b2af92a039ddfdc
Roz S <93357876+rstevenor@users.noreply.github.com>
2022-05-11 20:50:04 -0500

    Finished week5

--- CodingChallenges/Week3/CodeChallengeWeek3.java
+++ CodingChallenges/Week3/CodeChallengeWeek3.java
@@ -1,3 +1,5 @@
+package Week3;
+
 import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
---
+++ CodingChallenges/Week5/Week5.java
@@ -0,0 +1,11 @@
+package Week5;
+
+public class Week5 {
+    public static int addUp(int num) {
+        int sum = 0;
+        for(int i=0; i <= num; i++){
+            sum+= i;
+        }
+        return sum;
+    }
+}
